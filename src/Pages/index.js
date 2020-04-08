53b84eda40f4b3dc496a136d79c317a5dcf7d8c0
Ian J Miller <ianmiller347@gmail.com>
2020-04-08 01:05:21 -0400

    add color changer background click

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,9 +1,20 @@
 import React, { useState } from 'react';
 import './style.scss';
 
+const randomInt = (max) => Math.floor(Math.random() * Math.floor(max));
+
+const getRandomColorRGB = () => {
+  return `rgb(${randomInt(255)},${randomInt(255)},${randomInt(255)})`;
+}
+
 const Pages = () => {
+  const [color, setColor] = useState();
+
   return (
-    <div className="container">
+    <div 
+      className={`container container--${color}`} 
+      onClick={e => setColor(getRandomColorRGB())}
+      style={ { backgroundColor: color || 'white'}}>
       <h1 className="title">dadtini's</h1>
       <p>dads can drink too</p>
     </div>
