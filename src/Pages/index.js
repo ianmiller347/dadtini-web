cc7cc65e768020ebf68b9bc9927420a783631d7a
Ian J Miller <ianmiller347@gmail.com>
2020-04-08 00:01:04 -0400

    init the app

---
+++ src/Pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.scss';
+
+const Pages = () => {
+  return (
+    <div className="container">
+      <h1>dadtini's</h1>
+      <p>dads can drink too</p>
+    </div>
+  );
+};
+
+export default Pages;
