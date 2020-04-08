929d2c3b30313d744eec8901e5aeafbbc1f43ae8
Ian J Miller <ianmiller347@gmail.com>
2020-04-08 00:15:06 -0400

    center text

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.scss';
 
 const Pages = () => {
   return (
     <div className="container">
-      <h1>dadtini's</h1>
+      <h1 className="title">dadtini's</h1>
       <p>dads can drink too</p>
     </div>
   );
