1eb76b7ce117d0968801f16465b5af7d24080887
Ian J Miller <ianmiller347@gmail.com>
2020-04-18 17:02:55 -0400

    commit the image with credit to prod who cares

--- src/components/DadtiniGenerator/DadtiniDisplay.js
+++ src/components/DadtiniGenerator/DadtiniDisplay.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 const DadtiniDisplay = ({ drink }) => {
+  const [credit, setCredit] = useState('');
+
   return (
     <div className="dadtini-display">
-      <h2>{drink.name}</h2>
       <div className="dadtini-display__image-container">
         {drink.image && 
-          <img className="dadtini-display__image" src={drink.image} alt={drink.name} />
+          <img className="dadtini-display__image" src={drink.image} alt={drink.name} title={`Author: ${drink.imageTitle}`} />
+        }
+        {credit &&
+          <div className="dadtini-display__image-credit">{credit}</div>
+        }
+        {drink.imageTitle &&
+          <button className="dadtini-display__image-set-credit" onClick={() => setCredit(drink.imageTitle)}>Image credit</button>
         }
       </div>
-      <p>{drink.name} is a dadtini.</p>
-      <p>{drink.description}</p>
+      <div className="dadtini-display__description">
+        <h2>{drink.name}</h2>
+        <p>{drink.name} is a dadtini.</p>
+        <p>{drink.description}</p>
+      </div>
     </div>
   );
 };
--- src/components/DadtiniGenerator/dadtini-data.js
+++ src/components/DadtiniGenerator/dadtini-data.js
@@ -1,37 +1,44 @@
 export const dadDrinks = [
   {
     name: 'Manhattan',
-    image: '',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a1/A_Manhattan.jpg/800px-A_Manhattan.jpg',
+    imageTitle: 'By Graeme Maclean from Glasgow, UK - manhattan, CC BY 2.0, https://commons.wikimedia.org/w/index.php?curid=45273416',
     description: `A classic drink for dads.`,
   },
   {
     name: 'Old Fashioned',
     image: '',
+    imageTitle: '',
     description: `A dad's favorite drink by far.`,
   },
   {
     name: 'Bushwacker',
     image: '',
+    imageTitle: '',
     description: 'Just a dad relaxing on the beach.',
   },
   {
     name: 'Cuba Libre',
     image: '',
+    imageTitle: '',
     description: 'Rum and Coke with lime juice? "Yes" -- dads',
   },
   {
     name: 'Daquiri',
     image: '',
+    imageTitle: '',
     description: 'Dads who read Hemingway and like to play it cool.',
   },
   {
     name: 'Mount Gay & Tonic',
     image: '',
+    imageTitle: '',
     description: 'Dads who own a sailboat all drink this dadtini classic.',
   },
   {
     name: 'Martini',
     image: '',
+    imageTitle: '',
     description: 'Shaken. Not stirred. Dads who have watched James Bond before probably.',
   },
 ];
